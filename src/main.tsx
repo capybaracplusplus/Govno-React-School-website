8e8948cab6cdb23350b93382570e56e6ee349813
Dima <dimak12k@yandex.ru>
2024-10-01 16:36:22 +0300

    CI fix v4

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/About/About'; // Импорт страницы "О нас"
+import About from './pages/About/About';
 import Header from './Header/Header.tsx';
 import Footer from './Footer/Footer.tsx';
 import Contacts from './pages/Contacts/contacts.tsx';
