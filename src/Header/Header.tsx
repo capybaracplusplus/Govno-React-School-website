2c665dcc8f57e4de8e5d25ace98f0e3072a13433
Dima <dimak12k@yandex.ru>
2024-10-03 01:57:17 +0300

    correct adaptive

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -33,19 +33,24 @@ function Header() {
         setIsOpen(!isOpen);
     }
     return (
-        <header className="hearder_container">
-            <div className='hearder'>
-                <nav className='hearder_nav'>
-                    <div className='hearder_logo'>
-                        <img src={faviconSrc} alt="Company Logo"></img>
-                    </div>
+        // hearder_container          className="container h-10"
+        <header>
+            {/* className='hearder' */}
+            {/* hearder_nav */}
+            <nav className=' p-5'>
+                <div className="space-x-8 container mx-auto flex justify-between items-center">
+                    {/* hearder_logo */}
+
+                    <img className="mr-0.6" src={faviconSrc} alt="Company Logo"></img>
+
                     <button id="menu-btn" className="block md:hidden focus:outline-none" onClick={clickBtnMenu}>
                         <svg className="w-8 h-8 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                         </svg>
                     </button>
-                    <div className="hidden md:flex space-x-8">
+
+                    <div className="hidden md:flex space-x-8 ml-auto pr-10">
                         <Button name="Главная" link="/" />
                         <Button name="О нас" link="/about" />
                         <Button name="Обучение" link="/education" />
@@ -56,34 +61,37 @@ function Header() {
                         <Button name="Авторизоваться" link="/login" />
                     </div>
 
-
-                </nav>
-
-                {isOpen && (
-                    <div id="mobile-menu" className="md:hidden bg-white shadow-md mobileMenu">
-                        <Button className="block px-4 py-2" name="Главная" link="/" />
-                        <Button className="block px-4 py-2" name="О нас" link="/about" />
-                        <Button className="block px-4 py-2" name="Обучение" link="/education" />
-                        <Button className="block px-4 py-2" name="Педсостав" link="/staff" />
-                        <Button className="block px-4 py-2" name="Вакансии" link="/vacancies" />
-                        <Button className="block px-4 py-2" name="Новости" link="/news" />
-                        <Button className="block px-4 py-2" name="Контакты" link="/contacts" />
-                        <Button className="block px-4 py-2" name="Авторизоваться" link="/login" />
+                    <div className='hidden md:flex space-x-4 ml-8'>
+                        <a target="_blank" className="icon_h" href="https://discord.com/">
+                            <img src="https://img.icons8.com/?size=50&id=25627&format=png" alt="DS icon"></img>
+                        </a>
+                        <a target="_blank" className="icon_h" href="https://web.telegram.org/a/">
+                            <img src="https://img.icons8.com/?size=50&id=9R1sV3QvY18K&format=png" alt="TG icon"></img>
+                        </a>
+                        <a target="_blank" className="icon_h" href="https://vk.com/">
+                            <img src="https://img.icons8.com/?size=50&id=38514&format=png" alt="VK icon"></img>
+                        </a>
                     </div>
-                )}
-
-                <div className='hearder_icons'>
-                    <a target="_blank" className="icon_h" href="https://discord.com/">
-                        <img src="https://img.icons8.com/?size=50&id=25627&format=png" alt="DS icon"></img>
-                    </a>
-                    <a target="_blank" className="icon_h" href="https://web.telegram.org/a/">
-                        <img src="https://img.icons8.com/?size=50&id=9R1sV3QvY18K&format=png" alt="TG icon"></img>
-                    </a>
-                    <a target="_blank" className="icon_h" href="https://vk.com/">
-                        <img src="https://img.icons8.com/?size=50&id=38514&format=png" alt="VK icon"></img>
-                    </a>
                 </div>
-            </div>
+
+            </nav>
+
+            {isOpen && (
+                // mobileMenu
+                <div id="mobile-menu" className="md:hidden bg-white shadow-md ">
+                    <Button className="block px-4 py-2" name="Главная" link="/" />
+                    <Button className="block px-4 py-2" name="О нас" link="/about" />
+                    <Button className="block px-4 py-2" name="Обучение" link="/education" />
+                    <Button className="block px-4 py-2" name="Педсостав" link="/staff" />
+                    <Button className="block px-4 py-2" name="Вакансии" link="/vacancies" />
+                    <Button className="block px-4 py-2" name="Новости" link="/news" />
+                    <Button className="block px-4 py-2" name="Контакты" link="/contacts" />
+                    <Button className="block px-4 py-2" name="Авторизоваться" link="/login" />
+
+                </div>
+            )}
+
+
         </header>
     );
 }
