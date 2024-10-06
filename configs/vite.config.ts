e5371fb5f9ef42c709bd5e7b915c9f02ecfebe6a
Dima <dimak12k@yandex.ru>
2024-10-07 00:57:23 +0300

    fix build

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -6,6 +6,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   base: './',
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   css: {
     postcss: path.resolve(__dirname, 'postcss.config.js'),
   },
